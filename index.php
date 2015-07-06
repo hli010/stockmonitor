6a0e7c3e945567b439aa38406ff387539c4ced18
hengLi <hli010@gmail.com>
2015-07-06 20:12:55 +0800

    newer version

--- index.php
+++ index.php
@@ -10,6 +10,16 @@
 </head>
 <body>
 
+<table class="example table-autosort table-autofilter table-autopage:1300 table-stripeclass:alternate table-page-number:t1page table-page-count:t1pages table-filtered-rowcount:t1filtercount table-rowcount:t1allcount" style="margin:0"><thead> <tr>
+
+</tr>
+
+</thead> <tbody>
+
+</tbody>
+<tr>
+<td>
+
 <script type="text/javascript" src="http://hq.sinajs.cn/list=s_sh000001,s_sz399001" charset="gb2312"> </script>
 <script type="text/javascript">
 var elements=hq_str_s_sh000001.split(",");
@@ -40,22 +50,27 @@ document.write(elements[0]+" : "+elements[1]+" (<font color="+color+
 
 
 document.write("<img src=\"http://image.sinajs.cn/newchart/min/n/s_sh000001.gif\" />");
+</script>
+</td>
+<td>
+<script type="text/javascript">
 document.write("<img src=\"http://image.sinajs.cn/newchart/daily/n/sh000001.gif\"/>");
 </script>
+
+</td>
+</tr>
+</table>
+
 <hr>
 
 
 
-<table class="example table-autosort table-autofilter table-autopage:1300 table-stripeclass:alternate table-page-number:t1page table-page-count:t1pages table-filtered-rowcount:t1filtercount table-rowcount:t1allcount" style="margin:0 auto"><thead> <tr>
-<th class="table-sortable:numeric table-sortable table-sorted-asc" > No </th>
+<table cellpadding="0" cellspacing="0" class="example table-autosort table-autofilter table-autopage:1300 table-stripeclass:alternate table-page-number:t1page table-page-count:t1pages table-filtered-rowcount:t1filtercount table-rowcount:t1allcount" style="margin:0"><thead> <tr>
 <th title="device ID" class="table-sortable:default table-sortable">  Name  </th>
 <th title="device ID" class="table-sortable:default table-sortable">  当前价格  </th>
 <th title="error code" class="table-sortable:default table-sortable"> Real K </th>
 <th title="error code" class="table-sortable:default table-sortable"> Day K </th>
-<th title="error code" class="table-sortable:default table-sortable"> 买 </th>
-<th title="error code" class="table-sortable:default table-sortable"> 卖 </th>
-<th title="error code" class="table-sortable:default table-sortable"> 日期 </th>
-<th title="error code" class="table-sortable:default table-sortable"> 时间 </th>
+
 </tr>
 
 </thead> <tbody>
@@ -121,12 +136,12 @@ foreach ($stock_name_list as $stock){
         $stname = substr($stock, 2); 
         echo "$stname";
 	echo "
-<tr class=\"\"><td>
-<a href=http://quote.eastmoney.com/$stock.html> $no </a> </td>
+<tr class=\"\">
+<td> <a href=http://quote.eastmoney.com/$stock.html> $no </a> <br>
 <script type=\"text/javascript\">
 var s_ele = hq_str_s_$stock.split(\",\");
 var elements=hq_str_$stock.split(\",\");
-document.write(\"<td><a href=http://stockpage.10jqka.com.cn/$stname/>\"+elements[0]+\"($stname)\"+\"</a></td>\");
+document.write(\"<a href=http://stockpage.10jqka.com.cn/$stname/>\"+elements[0]+\"($stname)\"+\"</a></td>\");
 var color=\"red\";
 if (s_ele[3] < 0) {
    color = \"green\";
@@ -135,10 +150,10 @@ if (s_ele[3] < 0) {
 var stock_value_ele = v_$stock.split(\"~\");
 
 
-document.write(\"<td><table brorder=\\\"0\\\">\");
-document.write(\"<tr><td width=\\\"100%\\\" nowrap>今日开盘价 </td><td>  \" +elements[1] +\"</td><tr>\");
-document.write(\"<tr><td>昨日收盘价 </td><td>  \" +elements[2] +\"</td><tr>\");
-document.write(\"<tr><td>当前价格 </td><td> <font color=\"+color+\"> \" +elements[3] 
+document.write(\"<td><table cellpadding=\\\"0\\\" cellspacing=\\\"0\\\" class=\\\"example table-autosort table-autofilter table-autopage:1300 table-stripeclass:alternate table-page-number:t1page table-page-count:t1pages table-filtered-rowcount:t1filtercount table-rowcount:t1allcount\\\" style=\\\"margin:0\\\">\");
+document.write(\"<tr><td nowrap>今日开盘 </td><td>  \" +elements[1] +\"</td>\");
+document.write(\"<td nowrap>昨日收盘 </td><td>  \" +elements[2] +\"</td><tr>\");
+document.write(\"<tr><td>当前价格 </td><td colspan=3> <font color=\"+color+\"> \" +elements[3] 
 	+\"(\"
 	+s_ele[1]
 	+\",\"
@@ -146,39 +161,50 @@ document.write(\"<tr><td>当前价格 </td><td> <font color=\"+color+\"> \" +ele
 	+\",\"
 	+s_ele[3]+\"%)\" 
 	+\"</font> </td><tr>\");
-document.write(\"<tr><td>今日最高价 </td><td>  \" +elements[4] +\"</td><tr>\");
-document.write(\"<tr><td>今日最低价 </td><td>  \" +elements[5] +\"</td><tr>\");
-document.write(\"<tr><td>市盈率   </td><td>  \" +stock_value_ele[39] +\"</td><tr>\");
-document.write(\"<tr><td>总市值   </td><td>  \" +stock_value_ele[45] +\"</td><tr>\");
-document.write(\"<tr><td>买一 </td><td>  \" +elements[6] +\"</td><tr>\");
-document.write(\"<tr><td>卖一  </td><td>  \" +elements[7] +\"</td><tr>\");
-var num = elements[8] / 100;
-document.write(\"<tr><td>成交的股票数  </td><td>  \" +num +\"</td><tr>\");
-var total = elements[9] / 10000; 
-document.write(\"<tr><td>成交金额  </td><td>  \" +total +\"</td><tr>\");
+document.write(\"<tr><td>今日最高 </td><td>  \" +elements[4] +\"</td>\");
+document.write(\"<td>今日最低 </td><td>  \" +elements[5] +\"</td><tr>\");
+document.write(\"<tr><td>市盈率   </td><td>  \" +stock_value_ele[39] +\"</td>\");
+document.write(\"<td>总市值   </td><td>  \" +stock_value_ele[45] +\"</td><tr>\");
+document.write(\"<tr><td>买一 </td><td>  \" +elements[6] +\"</td>\");
+document.write(\"<td>卖一  </td><td>  \" +elements[7] +\"</td><tr>\");
+var num = Math.round(elements[8] / 100);
+document.write(\"<tr><td>成交数  </td><td>  \" +num +\"</td>\");
+var total = Math.round(elements[9] / 10000); 
+document.write(\"<td>成交金额  </td><td>  \" +total +\"</td><tr>\");
+document.write(\"<tr><td colspan=4> \"+elements[30]+\"     \" +elements[31] +\"</td><tr>\");
+
+document.write(\"<tr>\");
+document.write(\" <td>\"+elements[11] +\"</td><td>  \" +Math.round(elements[10]/100) +\"</td> \");
+document.write(\" <td>\"+elements[21] +\"</td><td>  \" +Math.round(elements[20]/100) +\"</td><tr>\");
+document.write(\"</tr>\");
+
+document.write(\"<tr>\");
+document.write(\" <td>\"+elements[13] +\"</td><td>  \" +Math.round(elements[12]/100) +\"</td> \");
+document.write(\" <td>\"+elements[23] +\"</td><td>  \" +Math.round(elements[22]/100) +\"</td><tr>\");
+document.write(\"</tr>\");
+
+document.write(\"<tr>\");
+document.write(\" <td>\"+elements[15] +\"</td><td>  \" +Math.round(elements[14]/100) +\"</td> \");
+document.write(\" <td>\"+elements[25] +\"</td><td>  \" +Math.round(elements[24]/100) +\"</td><tr>\");
+document.write(\"</tr>\");
+
+document.write(\"<tr>\");
+document.write(\" <td>\"+elements[17] +\"</td><td>  \" +Math.round(elements[16]/100) +\"</td> \");
+document.write(\" <td>\"+elements[27] +\"</td><td>  \" +Math.round(elements[26]/100) +\"</td><tr>\");
+document.write(\"</tr>\");
+
+document.write(\"<tr>\");
+document.write(\" <td>\"+elements[19] +\"</td><td>  \" +Math.round(elements[18]/100) +\"</td> \");
+document.write(\" <td>\"+elements[29] +\"</td><td>  \" +Math.round(elements[28]/100) +\"</td><tr>\");
+document.write(\"</tr>\");
+
 document.write(\"</table></td>\");
 
 document.write(\"<td><img src=\\\"http://image.sinajs.cn/newchart/min/n/$stock.gif\\\" /></td>\");
 document.write(\"<td><img src=\\\"http://image.sinajs.cn/newchart/daily/n/$stock.gif\\\" /></td>\");
 
-document.write(\"<td><table brorder=\\\"0\\\">\");
-document.write(\"<tr><td>\"+elements[11] +\"</td><td>  \" +elements[10] +\"</td><tr>\");
-document.write(\"<tr><td>\"+elements[13] +\"</td><td>  \" +elements[12] +\"</td><tr>\");
-document.write(\"<tr><td>\"+elements[15] +\"</td><td>  \" +elements[14] +\"</td><tr>\");
-document.write(\"<tr><td>\"+elements[17] +\"</td><td>  \" +elements[16] +\"</td><tr>\");
-document.write(\"<tr><td>\"+elements[19] +\"</td><td>  \" +elements[18] +\"</td><tr>\");
-document.write(\"</table></td>\");
 
-document.write(\"<td><table>\");
-document.write(\"<tr><td>\"+elements[21] +\"</td><td>  \" +elements[20] +\"</td><tr>\");
-document.write(\"<tr><td>\"+elements[23] +\"</td><td>  \" +elements[22] +\"</td><tr>\");
-document.write(\"<tr><td>\"+elements[25] +\"</td><td>  \" +elements[24] +\"</td><tr>\");
-document.write(\"<tr><td>\"+elements[27] +\"</td><td>  \" +elements[26] +\"</td><tr>\");
-document.write(\"<tr><td>\"+elements[29] +\"</td><td>  \" +elements[28] +\"</td><tr>\");
-document.write(\"</table></td>\");
 
-document.write(\"<td>\"+elements[30]+\"</td>\");
-document.write(\"<td>\"+elements[31]+\"</td>\");
 </script>
 </tr>
 ";
